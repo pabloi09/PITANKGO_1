997afbc8f5fe96f7c6df768880375bc692f8930e
pablo <pablo.arribas100@gmail.com>
2019-05-01 12:59:19 +0200

    Librerías inútiles

--- piTankGo_1/CamaraController.h
+++ piTankGo_1/CamaraController.h
@@ -2,8 +2,6 @@
 #define CAMARACONTROLLER_H_
 
 #include "piTankGoLib.h"
-#include <arpa/inet.h>
-#include <sys/socket.h>
 #include <netdb.h>
 #include <ifaddrs.h>
 #include <stdio.h>
@@ -11,11 +9,11 @@
 #include <unistd.h>
 #include <string.h>
 
-#define START "lxterminal -e \"./camara.sh\" & PID=$! "	//Comando de inicio del programa
+#define START "lxterminal -e \"./camara.sh\" "	//Comando de inicio del programa
 #define END "./end"			//Comando de fin del programa
 
 pid_t pid;
-void *start();	//Funcion que inicia la difusión de video
-void *end();    //Funcion que acaba la difusión de video
+void *start();	//Funcion que inicia la difusiï¿½n de video
+void *end();    //Funcion que acaba la difusiï¿½n de video
 void *getIP();  //Funcion que muestra por pantalla la IP de la raspberry
 #endif /* CAMARACONTROLLER_H_ */
